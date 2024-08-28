abf317713982b394411353f8ccf49d1300f9b59f
AjayFrancisTechversant <ajay.francis@techversantinfotech.com>
2024-08-28 14:23:56 +0530

    bookmarksorting halfway

--- src/Components/HotelsContainer/index.tsx
+++ src/Components/HotelsContainer/index.tsx
@@ -1,13 +1,14 @@
-import {FlatList, ActivityIndicator} from 'react-native';
+import {FlatList, ActivityIndicator, Alert} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 import {useScreenContext} from '../../Contexts/ScreenContext';
 import HotelCard from '../HotelCard';
 import StaticVariables from '../../Preferences/StaticVariables';
 import ColorPalette from '../../Assets/Themes/ColorPalette';
-import styles from './style';
 import {useAppSelector} from '../../hooks/hooks';
 import {PrefenceType} from '../../Redux/Slices/UserDetailsSlice';
+import styles from './style';
 
 export type HotelType = {
   name: string;
@@ -24,10 +25,28 @@ const HotelsContainer = () => {
   const [hotels, setHotels] = useState<HotelType[]>(
     StaticVariables.EMPTY_ARRAY,
   );
+  const currentUserId = auth().currentUser?.uid;
+  const [bookmarkedHotelIds, setBookmarkedHotelIds] = useState<string[]>(
+    StaticVariables.EMPTY_ARRAY,
+  );
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection('bookmarks')
+      .doc(currentUserId)
+      .onSnapshot(documentSnapshot => {
+        setBookmarkedHotelIds(documentSnapshot.data()?.bookmarkedHotelIds);
+      });
+    return () => subscriber();
+  }, []);
 
   useEffect(() => {
     fecthHotels();
   }, [preferenceFromRedux]);
+
+  useEffect(() => {
+    sortBasedOnBookmarks();
+  }, [bookmarkedHotelIds]);
+
   const fecthHotels = () => {
     if (preferenceFromRedux) {
       setHotels(StaticVariables.EMPTY_ARRAY);
@@ -39,6 +58,17 @@ const HotelsContainer = () => {
           setHotels(querySnapshot.docs.map((i: any) => i.data())),
         );
     }
+  };
+
+  const sortBasedOnBookmarks = () => {
+    const tempArr: HotelType[] = [];
+    hotels.map(hotel => {
+      if (bookmarkedHotelIds.find(i => i == hotel.id)) {
+        tempArr.unshift(hotel)
+      }else tempArr.push(hotel)
+      setHotels(tempArr)
+    });
+
   };
   const screenContext = useScreenContext();
   const screenStyles = styles(
