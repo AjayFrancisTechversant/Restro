dd7a3f749be77cf618dbe6673df1daea088e8214
AjayFrancisTechversant <ajay.francis@techversantinfotech.com>
2024-09-05 11:03:10 +0530

    same

--- src/Components/HeaderComponent/index.tsx
+++ src/Components/HeaderComponent/index.tsx
@@ -5,13 +5,18 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import {useNavigation} from '@react-navigation/native';
 import {useScreenContext} from '../../Contexts/ScreenContext';
 import StaticVariables from '../../Preferences/StaticVariables';
+import LogoutComponent from '../LogoutComponent';
 import styles from './style';
 
 type HeaderComponentPropsType = {
   color: ColorValue;
+  inProfileScreen?: boolean;
 };
 
-const HeaderComponent: React.FC<HeaderComponentPropsType> = ({color}) => {
+const HeaderComponent: React.FC<HeaderComponentPropsType> = ({
+  color,
+  inProfileScreen,
+}) => {
   const navigation: any = useNavigation();
   const screenContext = useScreenContext();
   const screenStyles = styles(
@@ -26,12 +31,16 @@ const HeaderComponent: React.FC<HeaderComponentPropsType> = ({color}) => {
       <TouchableOpacity onPress={() => navigation.openDrawer()}>
         <Ionicons name="menu" color={color} size={40} />
       </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate(StaticVariables.ProfileScreen as never)
-        }>
-        <FontAwesome name="user" color={color} size={40} />
-      </TouchableOpacity>
+      {!inProfileScreen ? (
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate(StaticVariables.ProfileScreen as never)
+          }>
+          <FontAwesome name="user" color={color} size={40} />
+        </TouchableOpacity>
+      ) : (
+        <LogoutComponent />
+      )}
     </View>
   );
 };
